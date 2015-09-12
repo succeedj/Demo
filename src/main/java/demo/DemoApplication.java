1ad8dc73c03014d6a8f964ba5d7bbd28aa79d3f8
Succeed Jabangwe <succeedj@gmail.com>
2015-09-11 23:19:29 -0500

    Fixed bug

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	//fixed bug
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
